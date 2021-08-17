e4fb275fb2c9a395f49831b315f99dbf1852e00e
reemsh2000 <sh951652@gmail.com>
2021-08-17 17:50:06 +0300

    edit in tables in data base

--- server/database/init.sql
+++ server/database/init.sql
@@ -1,14 +1,16 @@
 BEGIN;
-
+DROP TABLE IF EXISTs Artist,Painting;
 CREATE TABLE Artist (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(200)
+    name VARCHAR(200) UNIQUE
 );
 CREATE TABLE Painting(
     id SERIAL PRIMARY KEY,
-    artist_id INTEGER REFERENCES Artist(id),
+    artist_name VARCHAR REFERENCES Artist(name),
     painting_name VARCHAR(200),
     image_url TEXT
 );
-INSERT INTO Artist (name) VALUES ('RAWAND');
+INSERT INTO Artist (name) VALUES ('RAWAND'), ('reem');
+INSERT INTO Painting (artist_name,painting_name,image_url) VALUES ('reem','SUNSHINE','WWW');
+
 COMMIT;
