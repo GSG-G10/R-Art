d47899515adbc8adc1d785e79429a278f2bd750f
reemsh2000 <sh951652@gmail.com>
2021-08-17 17:49:31 +0300

    add post data function

---
+++ server/database/quires/postData.js
@@ -0,0 +1,12 @@
+const connection = require('../connection');
+
+const postData = (artistName, paintingName, imageUrl) => {
+  const sql = {
+    text: `INSERT INTO Painting (artist_name,painting_name,image_url)
+    VALUES ($1, $2, $3) RETURNING *`,
+    values: [artistName, paintingName, imageUrl],
+  };
+  return connection.query(sql)
+    .then((data) => data.rows)
+    .catch((error) => error);
+};
