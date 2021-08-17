1019f53664b2c583b65c507b1c3008e86743a131
rawand1998 <rawandgaradh@gmail.com>
2021-08-17 14:34:20 +0300

    create database

---
+++ server/database/connection.js
@@ -0,0 +1,10 @@
+// const { Pool } = require('pg');
+// require('env2')('../config.env');
+
+// const { DATABASE_URL } = process.env;
+// const options = {
+//   connectionString: DATABASE_URL,
+//   ssl: false,
+
+// };
+// module.exports = new Pool(options);
---
+++ server/database/init.js
@@ -0,0 +1,11 @@
+// const { join } = require('path');
+// const { readFileSync } = require('fs');
+// //const connection = require('./connection');
+
+// const sql = readFileSync(join(__dirname, 'init.sql')).toString();
+// console.log(sql);
+
+// connection
+//   .query(sql)
+//   .then(() => console.log('db bulid sucess'))
+//   .catch(() => console.log('db bulid fails'));
