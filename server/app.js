57b0008d3fa839516f72cdeff2bb541b86b1e99a
reemsh2000 <sh951652@gmail.com>
2021-08-18 12:42:07 +0300

    pop up window to add post

--- server/app.js
+++ server/app.js
@@ -6,9 +6,9 @@ const router = require('./controllers/index');
 
 const app = express();
 app.set('port', process.env.PORT || 7070);
-app.use(router);
 app.use(compression());
 app.use(express.static(join(__dirname, '..', 'public'), { maxAge: '30d' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(router);
 module.exports = app;
