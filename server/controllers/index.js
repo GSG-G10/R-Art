d606109b4e29b8b0fe86087623f0973ea8b82692
rawand1998 <rawandgaradh@gmail.com>
2021-08-18 11:11:41 +0300

    post route

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getData } = require('../database/queries');
+const { getData, postData } = require('../database/queries');
 
 const router = express.Router();
 router.get('/show', (req, res) => {
@@ -7,5 +7,9 @@ router.get('/show', (req, res) => {
     .then((result) => res.json(result))
     .catch((err) => console.log(err));
 });
-
+router.post('/addPost', (req, res) => {
+  postData(req.body.artist_name, req.body.painting_name, req.body.image_url)
+    .then(result => res.redirect('/'))
+    .catch(err => res.status(500).json({ msg: 'Internal Server Error' }));
+});
 module.exports = router;
